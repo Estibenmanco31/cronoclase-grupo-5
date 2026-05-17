d6849426db1cb58abd9057bd7aaf933531f2d688
GGP113 <paulaag316@gmail.com>
2026-05-17 10:20:40 -0500

    fixed repository

--- src/main/java/com/grupo5/cronoclase/repository/CursoRepository.java
+++ src/main/java/com/grupo5/cronoclase/repository/CursoRepository.java
@@ -1,10 +1,11 @@
-e package com.grupo5.cronoclase.repository;
+package com.grupo5.cronoclase.repository;
 
 
 import com.grupo5.cronoclase.model.entity.*;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.*;
+
 
 
 
